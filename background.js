435bfbff5e7e1c7019e298889daf7eb5758c7ba9
Andrea Procopio <andreproco@gmail.com>
2023-01-28 17:48:04 +0100

    Added interrupted and fixed lang problem

--- background.js
+++ background.js
@@ -1,9 +1,32 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
+  chrome.tts.getVoices(function(voices) {
+    console.log(voices);
+  });
+
+  console.log(request.pageLang);
+  
   if (request.textToRead && request.pageLang && request.readingSpeed) {
 
+    let voiceName;
+    let language;
+
+    if (request.pageLang == 'it') {
+      voiceName = 'Google Italiano';
+      language = 'it-IT';
+    } else {
+      voiceName = 'Google US English';
+      language = 'en-US';
+    }
+
+    console.log(voiceName);
+
+    // Text to speech the received text
     chrome.tts.speak(request.textToRead, {
       'rate': request.readingSpeed,
-      'lang': request.pageLang,
+      'lang': language,
+      'voiceName': voiceName,
+      // Events required: end and interrupted
       requiredEventTypes: ['end'],
       onEvent: function(event) {
         if(event.type === 'end') {
--- content.js
+++ content.js
@@ -16,25 +16,51 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 
 document.addEventListener('click', function(event) {
   
+  // Check if the extension is on or off
   if(state === 'on') {
     
+    // If clicked on a non-text element return
     if (event.target.tagName === 'BODY') {
       return;
     }
     
+    // Grab the language of the page, the text content of the clicked element and set its background to gray.
     const pageLang = document.documentElement.getAttribute('lang');
     const textToRead = event.target.textContent;
     let currentReadedElement = event.target;
-    event.target.style.backgroundColor = "lightgray";
+    currentReadedElement.style.backgroundColor = "lightgray";
+    currentReadedElement.setAttribute("data-current-element", "true");
+
+    // Max Char
+    // const maxChar = 200;
+    // if (textToRead.length > maxChar) {
+    //   // splitting long text into chunks
+    //   const chunks = [];
+    //   let i, j;
+    //   for (i = 0, j = textToRead.length; i < j; i += maxChar) {
+    //       chunks.push(textToRead.slice(i, i + maxChar));
+    //   }
+    // }
+
+    // remove the data-current-element attribute from any other element that has it
+    let elements = document.querySelectorAll("[data-current-element=true]");
+    elements.forEach(el => {
+        if(el !== currentReadedElement) {
+          el.style.backgroundColor = '';
+          el.removeAttribute("data-current-element");
+        }
+    });
   
     if (textToRead) {
+      // Send a message to background.js with the text to read, the speed, the clicked element and the language.
       chrome.runtime.sendMessage({
         textToRead: textToRead,
         pageLang: pageLang,
         currentReadedElement: currentReadedElement,
         readingSpeed: speed
-      }, function(response) {
-        currentReadedElement.style.backgroundColor = "";
+      }, function (response){
+        currentReadedElement.style.backgroundColor = '';
+        currentReadedElement.removeAttribute("data-current-element");
       });
     }
   } else if (state === 'off') {
